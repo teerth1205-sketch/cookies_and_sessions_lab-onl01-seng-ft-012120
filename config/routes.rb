9b594f310ff0181fea0facfdae59549ee4f8149e
Teerth Patel <teerth1205@gmail.com>
2020-04-07 22:26:50 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   
-  resources :products, only: [:index, :add]
+  resources :products, only: [:index]
+  
+  post "/" => "products#add"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
