6248d0eda5c0fe6161a2fa1cf17aebad6fe4c63f
Teerth Patel <teerth1205@gmail.com>
2020-04-07 22:44:17 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   helper_method :cart
   def cart 
-    session[:cart] ||= []
+    session[:carts] ||= []
   end 
 end
